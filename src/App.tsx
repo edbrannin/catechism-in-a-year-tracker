86e792c659a3bdee9e9024ed8514959ae8942b0d
Ed Brannin <edbrannin@gmail.com>
2023-01-06 20:18:48 -0500

    Add a link to the Catechism in a Year page to the top

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,9 @@ function App() {
   return (
     <div className="App">
       <h1>Catehism in a Year Tracker</h1>
+      <p>
+        <a href="https://ascensionpress.com/ciy" className="link-button">Find the podcast and other resources here!</a>
+      </p>
       <ReadingPlanList
         progress={progress}
         readingPlanDays={readingPlan}
